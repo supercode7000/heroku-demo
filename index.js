f2f64ef1685c8d070e4aa9f478574bfee52f07ce
supercode7000 <anass@super-code.de>
2021-10-07 09:29:27 +0200

    project structure

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//app.set('views', './views')
+//view engine
+//app.set('view engine', 'ejs')
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`)
+})
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { title: 'home' })
+})
+app.get('/about', (req, res) => {
+    res.render('about.ejs')
+})
